6c3b6acde75af56e09454ccd84cae47afc548742
Myrrh Khan <myrrhkhan@gmail.com>
2023-07-21 02:31:40 -0400

    added clarification as to where settings.json is

--- src-tauri/src/get_and_add_vars.rs
+++ src-tauri/src/get_and_add_vars.rs
@@ -50,10 +50,6 @@ pub fn add_var(key: String, var_submission: String) -> Result<String, String> {
     }
     // Try to append variable
     let result = append(&key, &var_submission);
-    // match &result {
-    //   Ok(msg) => println!("From Rust:\n{}", msg),
-    //   Err(err_msg) => println!("From Rust:\n{}", err_msg)
-    // }
     return result;
   } else {
       return Err(String::from("Invalid input, contains null character or is empty."));
