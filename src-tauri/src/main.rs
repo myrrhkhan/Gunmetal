b58bf392f87c2805201c87ad6e636601dfe37344
Myrrh Khan <myrrhkhan@gmail.com>
2023-09-21 16:29:59 -0400

    backing up

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::env;
-
 use settings_utils::{gather_setting, get_config_path};
 mod add_vars;
 mod consts_and_errors;
@@ -14,7 +12,7 @@ fn main() {
         .invoke_handler(tauri::generate_handler![
             get_vars::get_vars,
             add_vars::add_var,
-            get_shell_location
+            get_shell_location,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
